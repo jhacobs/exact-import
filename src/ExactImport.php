7b17dbcf035a84ea8dd1dc799824c3d931c0a18b
Jacob Aretz <jacob.aretz@beeproger.com>
2020-07-11 21:48:05 +0200

    Fix exact import

--- src/ExactImport.php
+++ src/ExactImport.php
@@ -2,7 +2,6 @@
 
 namespace Exact;
 
-use Exact\Contracts\QueryProvider;
 use Exact\Exceptions\QueryProviderNotFoundException;
 use Illuminate\Database\Query\Builder;
 use Illuminate\Support\Collection;
@@ -17,22 +16,19 @@ abstract class ExactImport
     {
         $this->before();
 
-        $this->queryProvider($this->query())->chunk(function (Collection $items) {
+        $query = $this->query();
+
+        if (! $query instanceof Builder) {
+            throw new QueryProviderNotFoundException();
+        }
+
+        $query->chunk(config('exact.chunk_total'), function (Collection $items) {
             $this->import($items);
         });
 
         $this->after();
 
         return true;
-    }
-
-    protected function queryProvider($query): QueryProvider
-    {
-        if ($query instanceof Builder) {
-            return new QueryBuilderProvider($query);
-        }
-
-        throw new QueryProviderNotFoundException();
     }
 
     protected function before(): void
--- src/Queries/Query.php
+++ src/Queries/Query.php
@@ -2,11 +2,21 @@
 
 namespace Exact\Queries;
 
+use Illuminate\Database\ConnectionInterface;
 use Illuminate\Database\Query\Builder;
+use Illuminate\Database\Query\Grammars\Grammar;
+use Illuminate\Database\Query\Processors\Processor;
 use Illuminate\Support\Collection;
 
 abstract class Query extends Builder
 {
+    public function __construct(ConnectionInterface $connection, Grammar $grammar = null, Processor $processor = null)
+    {
+        parent::__construct($connection, $grammar, $processor);
+
+        $this->from($this->table());
+    }
+
     abstract public function table(): string;
 
     public function fields($fields): self
@@ -22,7 +32,6 @@ abstract class Query extends Builder
 
     public function fetch($columns = ['*']): Collection
     {
-        return $this->from($this->table())
-            ->get($columns);
+        return $this->get($columns);
     }
 }
